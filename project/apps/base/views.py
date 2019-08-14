1a50e283372cc57d2624034c960dfdeeba6182ed
slaily <slavov.iliyan96@gmail.com>
2019-08-14 23:38:19 +0300

    Define function based view for handling page not found error and create 404 HTML template

--- project/apps/base/views.py
+++ project/apps/base/views.py
@@ -1,4 +1,32 @@
 from django.views.generic.base import TemplateView
+from django.http import HttpResponseNotFound
+from django.template import (
+    Context,
+    Engine,
+    TemplateDoesNotExist,
+    loader
+)
+
+
+ERROR_404_TEMPLATE_NAME = 'base/404.html'
+
+
+def page_not_found(request, exception):
+    """
+    404 handler.
+
+    Template: `base/404.html`
+    """
+    try:
+        template = loader.get_template(ERROR_404_TEMPLATE_NAME)
+        body = template.render(request=request)
+    except TemplateDoesNotExist:
+        template = Engine().from_string(
+            '<h1>Not Found</h1>'
+            '<p>The requested URL was not found on this server.</p>'
+        )
+        body = template.render(Context({}))
+    return HttpResponseNotFound(body)
 
 
 class IndexView(TemplateView):
