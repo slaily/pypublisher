03c6e7ffa620678b1c24867ffd6b282582b0d971
slaily <slavov.iliyan96@gmail.com>
2019-08-27 18:39:35 +0300

    Create a test case for the contact view in the 'base' application

--- project/apps/base/tests/test_views.py
+++ project/apps/base/tests/test_views.py
@@ -1,4 +1,8 @@
+from unittest import TestCase
+
 from django.test import SimpleTestCase
+
+from project.apps.blog.views import ContactView
 
 
 class PageNotFoundViewTest(SimpleTestCase):
@@ -6,3 +10,19 @@ class PageNotFoundViewTest(SimpleTestCase):
         response = self.client.get('/404')
 
         self.assertEqual(response.status_code, 404)
+
+
+class ContactViewTest(TestCase):
+    def test_form_valid(self):
+        contact_view = ContactView()
+        contact_form = contact_view.form_class(
+         data={
+                'subject': 'Test Subject',
+                'message': 'Test Message',
+                'from_email': 'test@test.com'
+            }
+        )
+        contact_form.is_valid()
+        response = contact_view.form_valid(contact_form)
+
+        self.assertEqual(response.status_code, 302)
