fb4fecaa813adedb1047f1e70a0f52fb6b8cbce2
slaily <slavov.iliyan96@gmail.com>
2019-08-27 18:34:40 +0300

    Create a test case for the contact form functionality in the 'blog' application

---
+++ project/apps/blog/tests/test_forms.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+from django.core import mail
+
+from project.apps.blog import forms
+
+
+class ContactFormTest(TestCase):
+    def test_send_email(self):
+        form = forms.ContactForm(
+            data={
+                'subject': 'Test Subject',
+                'message': 'Test Message',
+                'from_email': 'test@test.com'
+            }
+        )
+        form.is_valid()
+        form.send_email()
+
+        # Test that one message has been sent.
+        self.assertEqual(len(mail.outbox), 1)
