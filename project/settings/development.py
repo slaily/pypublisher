1e62f68c2943dbcd2915dabdbfdb77f4c443862d
slaily <slavov.iliyan96@gmail.com>
2019-09-04 21:59:32 +0300

    Add email configurations in the development settings

--- project/settings/development.py
+++ project/settings/development.py
@@ -1,9 +1,31 @@
+import os
+
 from sys import argv
+
+from dotenv import load_dotenv
 
 from project.settings.base import *  # noqa
 
 
+load_dotenv(dotenv_path=DOTENV_FILE_PATH)  # noqa
+
 if 'test' in argv:
     DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3'}  # noqa
 
+
+# ===========================
+# HOSTING
+# ===========================
+
 ALLOWED_HOSTS = ['*']
+
+
+# ===========================
+# EMAIL
+# ===========================
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+
+EMAIL_PORT = os.getenv('EMAIL_PORT')
