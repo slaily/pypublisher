20794d6194ed63596027429e7c172566e4eacbbc
slaily <slavov.iliyan96@gmail.com>
2020-03-30 20:43:07 +0300

    Define a sitemap URL

--- project/urls.py
+++ project/urls.py
@@ -1,14 +1,31 @@
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from django.urls import (
     path,
     include
 )
 
+from project.apps.blog.sitemaps import (
+    ArticleSitemap,
+    StaticViewSitemap
+)
+
+
+sitemaps = {
+    'articles': ArticleSitemap,
+    'contact': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('project.apps.base.urls')),
     path('blog/', include('project.apps.blog.urls')),
+    path(
+        'sitemap.xml',
+        sitemap,
+        {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap'
+    )
 ]
 
 handler404 = 'project.apps.base.views.page_not_found'
