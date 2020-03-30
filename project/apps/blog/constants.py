4832b951bae96fe380b0338f94f698b2d4b227dc
slaily <slavov.iliyan96@gmail.com>
2020-03-30 17:32:59 +0300

    Add email template string in the constants

---
+++ project/apps/blog/constants.py
@@ -0,0 +1,16 @@
+EMAIL_TEMPLATE_STR = """
+Dear MyBrand,
+
+You've received a new email. sent from the application,
+including the following information:
+
+-----------------------------------------------------------
+    Email: {email}
+    Subject: {subject}
+    Message:
+
+    {message}
+-----------------------------------------------------------
+
+We've kindly ask you to respond as soon as you can.
+"""
